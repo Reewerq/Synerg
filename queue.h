a7242414ad0a2b37697300546f0ff83e0530c2f6
Reewerq <reewer23@gmail.com>
2025-04-03 11:08:03 +0300

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,1 +1,17 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <queue>
+
+class Queue {
+private:
+    std::queue<int> q;
+
+public:
+    void push(int value);  // Добавление элемента
+    int pop();             // Извлечение элемента
+    bool isEmpty() const;  // Проверка на пустоту
+};
+
+#endif // QUEUE_H
 
