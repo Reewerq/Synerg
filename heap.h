dfe618cd723ac466714c37bec25ad40e1d35e75c
Reewerq <reewer23@gmail.com>
2025-04-03 11:08:30 +0300

    Update heap.h

--- heap.h
+++ heap.h
@@ -1,1 +1,17 @@
+#ifndef HEAP_H
+#define HEAP_H
 
+#include <vector>
+#include <queue>
+
+class Heap {
+private:
+    std::priority_queue<int> heap;
+
+public:
+    void push(int value);  // Добавление элемента
+    int pop();             // Извлечение элемента
+    bool isEmpty() const;  // Проверка на пустоту
+};
+
+#endif // HEAP_H
