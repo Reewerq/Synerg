17fed266154bdaee255e041591ac984fb4bf1edd
Reewerq <reewer23@gmail.com>
2025-04-03 11:08:46 +0300

    Update binary_tree.h

--- binary_tree.h
+++ binary_tree.h
@@ -1,1 +1,23 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+class BinaryTree {
+private:
+    struct Node {
+        int value;
+        Node* left;
+        Node* right;
+        Node(int val) : value(val), left(nullptr), right(nullptr) {}
+    };
+
+    Node* root;
+
+public:
+    BinaryTree();
+    void push(int value);  // Добавление элемента
+    bool search(int value) const;  // Поиск элемента
+    int pop();  // Удаление минимального элемента
+};
+
+#endif // BINARY_TREE_H
 
